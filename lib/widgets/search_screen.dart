aded3d22206a14d189f79fc9deb678784fd2f951
unknown <akbromz@gmail.com>
2023-04-13 20:58:59 +0530

    search modified

--- lib/widgets/search_screen.dart
+++ lib/widgets/search_screen.dart
@@ -38,88 +38,96 @@ class SearchWidget extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Consumer<studentProvider>(
-      builder: (context, providerClass, child) {
-        return ListView.builder(
-          itemBuilder: (ctx, index) {
-            final data = providerClass.studentListProvider[index];
-            if (data.name.toLowerCase().contains(query.toLowerCase().trim())) {
-              return Column(
-                children: [
-                  ListTile(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: ((context) {
-                            return const ListStudents();
-                          }),
-                        ),
-                      );
-                    },
-                    title: Text(data.name),
-                    leading: CircleAvatar(
-                      backgroundImage: FileImage(File(data.photo)),
+    final studentListProvider = Provider.of<studentProvider>(context);
+    final searchResults = studentListProvider.studentListProvider
+        .where((student) =>
+            student.name.toLowerCase().contains(query.toLowerCase().trim()))
+        .toList();
+
+    return searchResults.isEmpty
+        ? const Center(
+            child: Text("Match not found"),
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              final data = searchResults[index];
+              if (data.name
+                  .toLowerCase()
+                  .contains(query.toLowerCase().trim())) {
+                return Column(
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: ((context) {
+                              return const ListStudents();
+                            }),
+                          ),
+                        );
+                      },
+                      title: Text(data.name),
+                      leading: CircleAvatar(
+                        backgroundImage: FileImage(File(data.photo)),
+                      ),
                     ),
-                  ),
-                  const Divider()
-                ],
-              );
-            } else {
-              return Container(
-                child: Text('askjdfhkashdfh'),
-              );
-            }
-          },
-          itemCount: providerClass.studentListProvider.length,
-        );
-      },
-    );
+                    const Divider()
+                  ],
+                );
+              } else {
+                return Container();
+              }
+            },
+            itemCount: searchResults.length,
+          );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Consumer<studentProvider>(
-      builder: (context, providerClass, child) {
-        return ListView.builder(
-          itemBuilder: (ctx, index) {
-            final data = providerClass.studentListProvider[index];
-            if (data.name.toLowerCase().contains(query.toLowerCase().trim())) {
-              return Column(
-                children: [
-                  ListTile(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: ((context) {
-                            return DisplayStudent(
-                                name: data.name,
-                                age: data.age,
-                                address: data.address,
-                                number: data.phnNumber,
-                                index: index,
-                                photo: data.photo);
-                          }),
-                        ),
-                      );
-                    },
-                    title: Text(data.name),
-                    leading: CircleAvatar(
-                      backgroundImage: FileImage(File(data.photo)),
+    final studentList = Provider.of<studentProvider>(context);
+    final searchResults = studentList.studentListProvider;
+    return searchResults.isEmpty
+        ? const Center(
+            child: Text("Match not found"),
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              final data = searchResults[index];
+              if (data.name
+                  .toLowerCase()
+                  .contains(query.toLowerCase().trim())) {
+                return Column(
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: ((context) {
+                              return DisplayStudent(
+                                  name: data.name,
+                                  age: data.age,
+                                  address: data.address,
+                                  number: data.phnNumber,
+                                  index: index,
+                                  photo: data.photo);
+                            }),
+                          ),
+                        );
+                      },
+                      title: Text(data.name),
+                      leading: CircleAvatar(
+                        backgroundImage: FileImage(File(data.photo)),
+                      ),
                     ),
-                  ),
-                  const Divider()
-                ],
-              );
-            } else {
-              return Container(
-                child: Text('askjdfhkashdfh'),
-              );
-            }
-          },
-          itemCount: providerClass.studentListProvider.length,
-        );
-      },
-    );
+                    const Divider()
+                  ],
+                );
+              } else {
+                return Container();
+              }
+            },
+            itemCount: searchResults.length,
+          );
   }
 }
